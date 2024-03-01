c6c3d6072a89ff7ba8083d10ad7bafcf8e209941
ankit.chavhan <ankit.chavhan@techinfini.com>
2024-03-01 16:46:50 +0530

    FEAT: Added socket connection

--- app/messages/page.jsx
+++ app/messages/page.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+'use client'
+
+import { useEffect } from 'react';
+import io from 'socket.io-client';
+import axios from "axios"
+
 
 const MessagesPage = () => {
+    useEffect(() => {
+        const socket = io("http://localhost:3001");
+
+        // Handle events here
+
+        try {
+            
+            axios.get("/api/socket").then(response=>console.log("Messaging data=>",response.data))
+
+        } catch (error) {
+            console.log(error);
+        }
+        return () => {
+          socket.disconnect();
+        };
+      }, []);
     
   return (
     <div>MessagesPage</div>
